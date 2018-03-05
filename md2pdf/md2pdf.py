2807976f6fded708f60db7a3996556b8be1d8eda
YK-Orfeluna <k030913@kansai-u.ac.jp>
2018-03-05 11:30:18 +0900

    update md2pdf

--- md2pdf/md2pdf.py
+++ md2pdf/md2pdf.py
@@ -7,24 +7,48 @@ else :
 	exit("Sorry, this script only supports Python3.x. Please try it again.")
 
 from sys import argv
+from time import ctime
+from os.path import splitext
+from platform import system
+
+# check args, and set variable
 l = len(argv)
-if l == 3 :
+if l == 4 :
 	markdown = argv[1]
 	fontsize = argv[2]
+	output = argv[3]
+elif l == 3 :
+	markdown = argv[1]
+	fontsize = argv[2]
+	output = markdown
 elif l == 2 :
 	markdown = argv[1]
-	fontsize = 0.625
+	fontsize = 1.0
+	output = markdown
 else :
-	exit("Missing args. This script needs 1 or 2 args.\npython %s [$1: filename(markdown)] [$2: fontsize(em)]\n$1 is necessary to need, $2 is not nenessary to need.")
+	# missing args
+	exit("[%s]: Missing args. This script needs 1 or 2 args.\npython %s [$1: filename(markdown)] [$2: fontsize(em)] [$3: output-name]\n$1 is necessary to need."\
+		%(ctime(), argv[0]))
 
-#markdown = "ntt.md"
-#fontsize = 1.00
-
+# making CSS to change fontsize of PDF
 css = "pdf.css"
 with open(css, "w") as fd :
 	fd.write("body { font-size: %sem; }" %fontsize)
 
+# checking extension of output
+name, ext = splitext(output)
+if ext != ".pdf" :
+	output = name + ".pdf"
 
-call(["markdown-pdf", "-s", css ,markdown], shell=True)
+# run markdown-pdf
+call(["markdown-pdf", "-s", css, "-o", output , markdown], shell=True)
+print("[%s]: %s is wroten" %(ctime(), output))
+
+# delete CSS
+if system() == "Windows" :
+	call(["del", css], shell=True)
+else :
+	call(["rm", css], shell=True)
+	
 
 exit("done")
---
+++ md2pdf/sample.md
@@ -0,0 +1,25 @@
+# h1
+## h2
+### h3
+#### h4
+##### h5
+###### h6
+
+- itemize
+	- itemize
+- itemize
+- itemize
+
+<br>
+
+1. enumerate
+	1. enumerate
+2. enumerate
+3. enumerate
+
+<!-- comment out -->
+
+- *italic*
+- _italic_
+- **bold**
+- __bold__
